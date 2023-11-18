db5be5b62f1f71b7b5901678b2d38cbd93eb791f
barami62 <wodbs7758@naver.com>
2023-11-19 01:23:16 +0900

    init commit

---
+++ src/test/java/sparta/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
