47c3cbcfeb0a2ffc06518c28e57964b631029f87
barami62 <wodbs7758@naver.com>
2023-11-19 14:34:00 +0900

    feat : UserDetails 추가

---
+++ src/main/java/sparta/todoapp/global/config/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package sparta.todoapp.global.config.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import sparta.todoapp.domain.auth.entity.User;
+import sparta.todoapp.domain.auth.entity.UserRoleEnum;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+	private final User user;
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		UserRoleEnum role = user.getRole();
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getAuthority());
+
+		return List.of(authority);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
