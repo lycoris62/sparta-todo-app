2c6a8a45a44762d9da6c5c097676c3b8cbcf5f65
barami62 <wodbs7758@naver.com>
2023-11-19 11:28:34 +0900

    feat : BaseEntity 추가

---
+++ src/main/java/sparta/todoapp/domain/model/BaseEntity.java
@@ -0,0 +1,33 @@
+package sparta.todoapp.domain.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+
+/**
+ * 모든 엔티티가 가져야 할 속성을 가지는 엔티티.
+ * 생성일자와 수정일자를 저장하며, 상속으로 사용 가능.
+ */
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt; // 생성일자
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt; // 수정일자
+}
