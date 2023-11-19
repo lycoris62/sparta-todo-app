e82047f210e797791471bf51616f2d0496fe48b5
barami62 <wodbs7758@naver.com>
2023-11-20 01:21:57 +0900

    feat : 할일카드 수정 요청 DTO 추가

---
+++ src/main/java/sparta/todoapp/domain/todocard/dto/request/TodoCardEditRequestDto.java
@@ -0,0 +1,10 @@
+package sparta.todoapp.domain.todocard.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class TodoCardEditRequestDto {
+
+	private String title;
+	private String content;
+}
