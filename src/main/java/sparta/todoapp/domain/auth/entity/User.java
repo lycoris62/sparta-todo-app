c7c0cc368eed8185c1668c94f9c827e1ad69286b
barami62 <wodbs7758@naver.com>
2023-11-19 13:28:04 +0900

    feat : 유저 엔티티 추가

---
+++ src/main/java/sparta/todoapp/domain/auth/entity/User.java
@@ -0,0 +1,29 @@
+package sparta.todoapp.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sparta.todoapp.domain.model.BaseEntity;
+
+@Getter
+@Entity
+@Table(name = "users") // MySQL 에서 USER 는 예약어이므로 s를 붙임
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 에서는 기본 생성자가 필요하므로 최소 접근제어자로 생성
+public class User extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 10) // 4자 이상, 10자 이하
+	private String username;
+
+	@Column(nullable = false, length = 60) // 평문은 8자 이상, 15자 이하이지만, BCrypt 암호화 시 60자로 늘어남
+	private String password;
+}
