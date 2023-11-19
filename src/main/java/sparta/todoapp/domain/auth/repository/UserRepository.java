cd1503692080b7c99451952859741c71b8bacc90
barami62 <wodbs7758@naver.com>
2023-11-19 14:22:43 +0900

    feat : UserRepository 추가

---
+++ src/main/java/sparta/todoapp/domain/auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package sparta.todoapp.domain.auth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import sparta.todoapp.domain.auth.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+}
