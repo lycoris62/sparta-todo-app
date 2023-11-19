fdec2671317d014f69fe37b9987c8251789f5fd4
barami62 <wodbs7758@naver.com>
2023-11-20 02:17:29 +0900

    feat : 할일카드 응답 시 댓글 DTO 리스트 추가

--- src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardDetailResponseDto.java
+++ src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardDetailResponseDto.java
@@ -1,8 +1,10 @@
 package sparta.todoapp.domain.todocard.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import lombok.Getter;
+import sparta.todoapp.domain.comment.dto.response.CommentResponseDto;
 import sparta.todoapp.domain.todocard.entity.TodoCard;
 
 @Getter
@@ -12,11 +14,16 @@ public class TodoCardDetailResponseDto {
 	private final String content;
 	private final String username;
 	private final LocalDateTime createdAt;
+	private final List<CommentResponseDto> comments;
 
 	public TodoCardDetailResponseDto(TodoCard todoCard) {
 		this.title = todoCard.getTitle();
 		this.content = todoCard.getContent();
 		this.username = todoCard.getAuthor().getUsername();
 		this.createdAt = todoCard.getCreatedAt();
+		this.comments = todoCard.getCommentList()
+			.stream()
+			.map(CommentResponseDto::new)
+			.toList();
 	}
 }
