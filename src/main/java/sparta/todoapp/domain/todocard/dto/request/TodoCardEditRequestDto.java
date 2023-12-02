652612da20d64701f9e02a54b38b0994f374d9bd
barami62 <wodbs7758@naver.com>
2023-12-02 16:43:49 +0900

    Feat : 할일카드 DTO에 테스트용 생성을 위한 생성자 추가

--- src/main/java/sparta/todoapp/domain/todocard/dto/request/TodoCardEditRequestDto.java
+++ src/main/java/sparta/todoapp/domain/todocard/dto/request/TodoCardEditRequestDto.java
@@ -1,11 +1,13 @@
 package sparta.todoapp.domain.todocard.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * 할일카드 수정 요청 DTO
  */
 @Getter
+@AllArgsConstructor
 public class TodoCardEditRequestDto {
 
 	private String title;
