a446140a60fa41f8690663c9841b4b67d4b0ab28
barami62 <wodbs7758@naver.com>
2023-12-03 23:33:19 +0900

    Test : 중복된 유저네임으로 회원가입 실패 케이스 추가

--- src/test/java/sparta/todoapp/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/sparta/todoapp/domain/auth/controller/AuthControllerTest.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.nio.charset.StandardCharsets;
-import java.security.Principal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,21 +18,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import sparta.todoapp.global.config.security.MockSpringSecurityFilter;
 import sparta.todoapp.domain.auth.dto.AuthRequestDto;
-import sparta.todoapp.domain.auth.entity.User;
 import sparta.todoapp.domain.auth.service.AuthService;
-import sparta.todoapp.global.config.security.CustomUserDetails;
+import sparta.todoapp.global.config.security.MockSpringSecurityFilter;
 import sparta.todoapp.global.config.security.WebSecurityConfig;
 import sparta.todoapp.global.config.security.jwt.JwtUtil;
+import sparta.todoapp.global.error.exception.DuplicateUsernameException;
 
 @DisplayName("인증 컨트롤러 테스트")
 @MockBean(JpaMetamodelMappingContext.class)
@@ -49,8 +47,6 @@ import sparta.todoapp.global.config.security.jwt.JwtUtil;
 class AuthControllerTest {
 
 	private MockMvc mvc;
-
-	private Principal mockPrincipal;
 
 	@Autowired
 	private WebApplicationContext context;
@@ -87,6 +83,29 @@ class AuthControllerTest {
 			.andExpect(status().isOk());
 	}
 
+	@DisplayName("회원가입 실패 - 중복된 유저네임")
+	@Test
+	void signup_fail_duplicate_username() throws Exception {
+		// given
+		String username = "jaeyun";
+		String password = "12345678";
+		AuthRequestDto authRequestDto = new AuthRequestDto(username, password);
+
+		String jsonString = objectMapper.writeValueAsString(authRequestDto);
+		doThrow(new DuplicateUsernameException()).when(authService).signup(any(AuthRequestDto.class));
+
+		// when
+		ResultActions resultActions = mvc.perform(post("/api/auth/signup")
+			.content(jsonString)
+			.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+			.accept(MediaType.APPLICATION_JSON));
+
+		// then
+		resultActions
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("중복된 username 입니다."));
+	}
+
 	@DisplayName("로그인 성공")
 	@Test
 	void login() throws Exception {
@@ -96,10 +115,6 @@ class AuthControllerTest {
 		AuthRequestDto authRequestDto = new AuthRequestDto(username, password);
 
 		String jsonString = objectMapper.writeValueAsString(authRequestDto);
-
-		User testUser = User.createUser(username, password);
-		CustomUserDetails testUserDetails = new CustomUserDetails(testUser);
-		mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
 
 		given(authService.login(authRequestDto)).willReturn("token");
 
