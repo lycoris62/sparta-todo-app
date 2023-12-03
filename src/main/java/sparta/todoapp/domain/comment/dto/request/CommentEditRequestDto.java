832a6830427c68f56e37ed4071124a5aafee9df8
barami62 <wodbs7758@naver.com>
2023-12-03 21:26:58 +0900

    Feat : 요청 DTO에 테스트용 생성자 추가

--- src/main/java/sparta/todoapp/domain/comment/dto/request/CommentEditRequestDto.java
+++ src/main/java/sparta/todoapp/domain/comment/dto/request/CommentEditRequestDto.java
@@ -1,11 +1,15 @@
 package sparta.todoapp.domain.comment.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 댓글 수정 요청 DTO
  */
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentEditRequestDto {
 
 	private String content;
