70b3c34163df6f0ad7a1a3ce24285bbcf5b99ba6
barami62 <wodbs7758@naver.com>
2023-12-20 19:52:54 +0900

    Fix : JSON Serialize 에러 해결

--- src/main/java/sparta/todoapp/domain/auth/dto/request/AuthCheckUsernameRequestDto.java
+++ src/main/java/sparta/todoapp/domain/auth/dto/request/AuthCheckUsernameRequestDto.java
@@ -1,16 +1,18 @@
 package sparta.todoapp.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 /**
  * 회원가입에 사용되는 유저네임이 기존에 있는지를 확인하는 요청 DTO
  */
 @Getter
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthCheckUsernameRequestDto {
 
     @Pattern(regexp = "^[a-z0-9]{4,10}$") // a ~ z, 0 ~ 9 만 포함, 4이상 10이하
-    private final String username;
+    private String username;
 }
