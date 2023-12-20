842aaa0c0eaf33e07b10434b7374f15d712c664f
barami62 <wodbs7758@naver.com>
2023-12-20 19:05:32 +0900

    Fix : 제목을 500자로, 내용을 5000자로 제한

--- src/main/java/sparta/todoapp/domain/todocard/entity/TodoCard.java
+++ src/main/java/sparta/todoapp/domain/todocard/entity/TodoCard.java
@@ -1,9 +1,5 @@
 package sparta.todoapp.domain.todocard.entity;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,6 +10,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,10 +35,10 @@ public class TodoCard extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(length = 30, nullable = false)
+	@Column(length = 500, nullable = false)
 	private String title;
 
-	@Column(columnDefinition = "TEXT", nullable = false)
+	@Column(length = 5000, nullable = false)
 	private String content;
 
 	@Column
