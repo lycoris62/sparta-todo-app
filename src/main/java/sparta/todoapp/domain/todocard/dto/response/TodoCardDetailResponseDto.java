01147dc21c397afe1b963f41ef384c5cd1d538b6
barami62 <wodbs7758@naver.com>
2023-12-21 16:11:52 +0900

    Feat : DTO에 id 추가

--- src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardDetailResponseDto.java
+++ src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardDetailResponseDto.java
@@ -2,7 +2,6 @@ package sparta.todoapp.domain.todocard.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
 import lombok.Getter;
 import sparta.todoapp.domain.comment.dto.response.CommentResponseDto;
 import sparta.todoapp.domain.todocard.entity.TodoCard;
@@ -14,6 +13,7 @@ import sparta.todoapp.domain.todocard.entity.TodoCard;
 @Getter
 public class TodoCardDetailResponseDto {
 
+	private final Long id;
 	private final String title;
 	private final String content;
 	private final String username;
@@ -21,6 +21,7 @@ public class TodoCardDetailResponseDto {
 	private final List<CommentResponseDto> comments;
 
 	public TodoCardDetailResponseDto(TodoCard todoCard) {
+		this.id = todoCard.getId();
 		this.title = todoCard.getTitle();
 		this.content = todoCard.getContent();
 		this.username = todoCard.getAuthor().getUsername();
--- src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardSimpleResponseDto.java
+++ src/main/java/sparta/todoapp/domain/todocard/dto/response/TodoCardSimpleResponseDto.java
@@ -1,7 +1,6 @@
 package sparta.todoapp.domain.todocard.dto.response;
 
 import java.time.LocalDateTime;
-
 import lombok.Getter;
 import sparta.todoapp.domain.todocard.entity.TodoCard;
 
@@ -11,12 +10,14 @@ import sparta.todoapp.domain.todocard.entity.TodoCard;
 @Getter
 public class TodoCardSimpleResponseDto {
 
+	private final Long id;
 	private final String title;
 	private final String username;
 	private final LocalDateTime createdAt;
 	private final boolean isDone;
 
 	public TodoCardSimpleResponseDto(TodoCard todoCard) {
+		this.id = todoCard.getId();
 		this.title = todoCard.getTitle();
 		this.username = todoCard.getAuthor().getUsername();
 		this.createdAt = todoCard.getCreatedAt();
