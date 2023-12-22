e4dbc8a99f8877b9d837d2026e74b9fb12cfd81f
barami62 <wodbs7758@naver.com>
2023-12-22 11:27:22 +0900

    Feat : Like 엔티티 추가

--- src/main/java/sparta/todoapp/domain/todocard/entity/Like.java
+++ src/main/java/sparta/todoapp/domain/todocard/entity/Like.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -24,9 +25,11 @@ public class Like {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todocard_id")
     private TodoCard todoCard;
 
     @Builder
